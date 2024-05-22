907d5943d6f63cf6ef00fa9350db6927f589a526
Guilherme Vicentini <gdvicentini@gmail.com>
2024-05-22 10:00:20 -0300

    Inclusão de função anônima

--- 4_funcoes/script.js
+++ 4_funcoes/script.js
@@ -8,7 +8,26 @@ coletarAnoNascimento(nome, idade, anoAtual);
 coletarAnoNascimento("Mauricio", 15, 2024);
 coletarAnoNascimento("Claudio", 23, 2024);
 
+// Função convencional ou nomeada
 function coletarAnoNascimento(nomeRecebido, idadeRecebida, anoAtualRecebido) {
   let anoNascimento = anoAtualRecebido - idadeRecebida;
   console.log(`O ${nomeRecebido} nasceu em ${anoNascimento} !!`);
 }
+
+function somarValores(a, b) {
+  return a + b
+}
+
+console.log("------------------------------")
+// Função anônima
+let soma = function (a, b) {
+  return a + b
+}
+
+let resultadoSoma = somarValores(5, 10)
+
+console.log("Exemplo com função anônima: ", soma(15, 20))
+console.log("Exemplo com função convencional: ", resultadoSoma)
+
+console.log("------------------------------")
+// Função Seta (Arrow Function)
